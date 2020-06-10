0b431c4a8db75c607b92a8363d23b487eebebf9e
Ubuntu <ubuntu@ip-172-31-16-215.ec2.internal>
2020-06-10 19:16:48 +0000

    dash app update

--- create_topic.py
+++ create_topic.py
@@ -5,8 +5,11 @@ servers = server_name.servers
 
 admin_client = KafkaAdminClient(bootstrap_servers=servers, client_id='test')
 
-topic_list = []
 
-topic_list.append(NewTopic(name="cam_3", num_partitions=1, replication_factor=1))
+# all cam topics
+all_cams = ['cam_1','cam_2','cam_3','cam_4','cam_5','cam_6','cam_7','cam_8','cam_9','cam_10','cam_11','cam_12','cam_14','cam_15','cam_16']
+topic_list = []
+for item in all_cams:
+    topic_list.append(NewTopic(name=item, num_partitions=1, replication_factor=1))
 
 admin_client.create_topics(new_topics=topic_list, validate_only=False)
--- delete_topic.py
+++ delete_topic.py
@@ -5,7 +5,9 @@ servers = server_name.servers
 
 admin_client = KafkaAdminClient(bootstrap_servers=servers, client_id='test')
 
-topic_list = ['numtest']
+# all cam topics
+all_cams = ['cam_1','cam_2','cam_3','cam_4','cam_5','cam_6','cam_7','cam_8','cam_9','cam_10','cam_11','cam_12','cam_14','cam_15','cam_16']
+topic_list = all_cams
 
 admin_client.delete_topics(topics=topic_list)
 
