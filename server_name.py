6345c95d42976ba4128b81eb98e001e3ab326310
zm6148 <44382401+zm6148@users.noreply.github.com>
2020-06-11 20:38:39 -0400

    Delete server_name.py

--- server_name.py
+++
@@ -1,3 +0,0 @@
-servers = ['b-1.traffickafka.sn43u9.c7.kafka.us-east-1.amazonaws.com:9092',
-           'b-2.traffickafka.sn43u9.c7.kafka.us-east-1.amazonaws.com:9092',
-           'b-3.traffickafka.sn43u9.c7.kafka.us-east-1.amazonaws.com:9092']
