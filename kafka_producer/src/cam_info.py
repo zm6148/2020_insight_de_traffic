d2d2893f96c80746fbc5da12eadb83a3e61dee28
zm6148 <44382401+zm6148@users.noreply.github.com>
2020-06-26 18:33:11 -0400

    Update cam_info.py

--- kafka_producer/src/cam_info.py
+++ kafka_producer/src/cam_info.py
@@ -340,7 +340,3 @@ facings = ['south',
            'south',
            'weat',
            'east']
-
-servers = ['b-1.traffickafka.sn43u9.c7.kafka.us-east-1.amazonaws.com:9092',
-           'b-2.traffickafka.sn43u9.c7.kafka.us-east-1.amazonaws.com:9092',
-           'b-3.traffickafka.sn43u9.c7.kafka.us-east-1.amazonaws.com:9092']
